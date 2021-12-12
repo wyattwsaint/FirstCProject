eab589c7b86ba4461f9db1881e00bdca2b817438
Wyatt Saint <wmsaint17@gmail.com>
2021-12-11 21:45:04 -0500

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter monthly oil bill: ");
+            int averageMonthlyOilBill = Convert.ToInt32(Console.ReadLine());
+            firstMethod(averageMonthlyOilBill);
+        }
+        static void firstMethod(int oilBill)
+        {
+            int yearlyOilBill = oilBill * 12;
+            Console.WriteLine("Average yearly oil bill is: " + yearlyOilBill);
+            Console.ReadLine();
+        }
+    }
+}
